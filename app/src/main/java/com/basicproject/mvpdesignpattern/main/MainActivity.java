eaedd9f14e4214ae0d472d1706af9586813b6b42
admin <adhishta.ad@gmail.com>
2018-09-24 16:57:41 +0530

    unit test case for main presenter

--- app/src/main/java/com/basicproject/mvpdesignpattern/main/MainActivity.java
+++ app/src/main/java/com/basicproject/mvpdesignpattern/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.basicproject.mvpdesignpattern.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.basicproject.mvpdesignpattern.R;
 
@@ -14,5 +15,17 @@ public class MainActivity extends AppCompatActivity implements MainContract.MvpV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         mPresenter = new MainPresenter(this);
+    }
+
+    /////   View methods     /////
+
+    @Override
+    public void showSignInScreen() {
+        Toast.makeText(this, "You'll be directed to SignIn Screen ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showSignUpScreen() {
+        Toast.makeText(this, "You'll be directed to SignUp Screen ", Toast.LENGTH_SHORT).show();
     }
 }
