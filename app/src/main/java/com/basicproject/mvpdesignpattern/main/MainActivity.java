e6bfbe1f93a1c95f118fdf7a98a1cd43163b6b67
admin <adhishta.ad@gmail.com>
2018-09-27 15:54:30 +0530

    code styling with android version 3.2.0

--- app/src/main/java/com/basicproject/mvpdesignpattern/main/MainActivity.java
+++ app/src/main/java/com/basicproject/mvpdesignpattern/main/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.MvpV
         Toast.makeText(this, "You'll be directed to SignIn Screen ", Toast.LENGTH_SHORT).show();
     }
 
+
     @Override
     public void showSignUpScreen() {
         Toast.makeText(this, "You'll be directed to SignUp Screen ", Toast.LENGTH_SHORT).show();
